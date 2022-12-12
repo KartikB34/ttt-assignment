26410f29a9e01de45647bba907de6d7a8e2d45fb
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-12-12 23:09:48 +0530

    Data loaded successfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState("mello")
+
+  const handleClick = async () =>{
+
+    // console.log(data)
+    const text = await axios("https://www.terriblytinytales.com/test.txt")
+    // console.log(text.data)
+    setData(text.data)
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex items-center justify-center min-h-screen'>
+
+      <button 
+        onClick={handleClick} 
+        className='border-2 border-green-800 text-green-800 font-semibold rounded-full px-12 py-2 inline-block hover:bg-green-800 hover:text-white hover:cursor-pointer'
+      >
+        Submit
+      </button>
+
+      <div>
+        {data}
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
